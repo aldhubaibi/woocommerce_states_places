f53bb160451972cfaf151994909730f567351bfd
aldhubaibi <all1430@gmail.com>
2020-09-04 23:32:26 +0300

    Add files via upload

---
+++ trunk/states/YE.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [https://en.wikipedia.org/wiki/Governorates_of_Yemen]
+ *
+ * @author  Abdullah Aldhubaibi <abdullah.aldhubaibi@gmail.com.com> | https://yourwebsite.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+
+$states['XX'] = array(
+ 
+  'YE-01' => _x('Amanat Al Asimah','Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-02' => _x('Sana'a', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-03' => _x('Aden', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-04' => _x('Taiz', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-05' => _x('Ibb', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-06' => _x('Dhamar', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-07' => _x('Amran', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-08' => _x('Al Bayda', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-09' => _x('Al Hudaydah', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-10' => _x('Al Jawf', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-11' => _x('Al Mahwit', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-12' => _x('Hajjah', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-13' => _x('Ma'rib', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-14' => _x('Raymah', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-15' => _x('Sa'dah', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-16' => _x('Abyan', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-17' => _x('Al Mahrah', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-18' => _x('Al Dhale', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-19' => _x('Hadramaut', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-20' => _x('Socotra', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-21' => _x('Lahij', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  'YE-22' => _x('Shabwah', 'Governorate of Yemen', 'states-cities-and-places-for-woocommerce'),
+  
+);
